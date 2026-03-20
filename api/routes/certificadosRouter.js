35daaa241f60bc47cbd1da2db254d44f95b9be9b
jvxtorr <joaovictortwrp@gmail.com>
2026-03-20 07:10:47 -0300

    Ajuste
    configuração de credencias

--- api/routes/certificadosRouter.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { processarCertificado } from "../controllers/certificadosController.js";
-
-const router = Router();
-
-router.post("/processar", processarCertificado);
-
-export default router;
